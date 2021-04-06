2aa00fb928682796911c27dc9a3f96b4e21bb51c
Rafael Campos <rcampos@altus.cr>
2021-04-05 22:40:31 -0600

    Documents the non-standard return values of all modules in the collection

--- plugins/modules/pnp_workflow.py
+++ plugins/modules/pnp_workflow.py
@@ -313,5 +313,20 @@ EXAMPLES = r"""
 
 """
 
-RETURN = """
+RETURN = r"""
+dnac_response:
+  description: A dictionary with the response returned by the DNA Center Python SDK
+  returned: always
+  type: dict
+  sample: {"response": 29, "version": "1.0"}
+sdk_function:
+  description: The DNA Center SDK function used to execute the task
+  returned: always
+  type: str
+  sample: application_policy.get_application_sets
+missing_params:
+  description: Provided arguments do not comply with the schema of the DNA Center Python SDK function
+  returned: when the function request schema is not satisfied
+  type: array
+  sample:
 """
