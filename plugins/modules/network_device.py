0abb778190d379616a10fb399d851076722f5b65
William Astorga <wastorga@altus.co.cr>
2021-05-26 16:45:52 -0600

    Update internal LICENSE back for sanity check

--- plugins/modules/network_device.py
+++ plugins/modules/network_device.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2021, Cisco Systems
-# MIT License (see LICENSE)
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 DOCUMENTATION = r"""
 ---
