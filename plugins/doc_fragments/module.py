ec7ca165da8fccdf04eea5e04e79e0b0f0bc75c4
William Astorga <wastorga@altus.co.cr>
2021-10-28 12:54:35 -0600

    Add note explaining connection variables

--- plugins/doc_fragments/module.py
+++ plugins/doc_fragments/module.py
@@ -55,5 +55,6 @@ options:
         default: true
 notes:
     - "Does not support C(check_mode)"
-    - "The plugin runs on the control node and does not use any ansible connection plugins, but instead the embedded connection manager from the Python dnacentersdk."
+    - "The plugin runs on the control node and does not use any ansible connection plugins, but instead the embedded connection manager from the Cisco DNAC Python SDK"
+    - "The parameters starting with dnac_ are used by the Cisco DNAC Python SDK to establish the connection"
 '''
