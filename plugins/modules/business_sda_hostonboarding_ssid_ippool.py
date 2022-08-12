1ea4dbe4ab35d78c46b905fa3af80792843744d4
Bryan Vargas <bvargas@altus.cr>
2022-08-12 12:48:10 -0600

    add_ssid_to_ip_pool_mapping was added

--- plugins/modules/business_sda_hostonboarding_ssid_ippool.py
+++ plugins/modules/business_sda_hostonboarding_ssid_ippool.py
@@ -9,7 +9,8 @@ DOCUMENTATION = r"""
 module: business_sda_hostonboarding_ssid_ippool
 short_description: Resource module for Business Sda Hostonboarding Ssid Ippool
 description:
-- Manage operation update of the resource Business Sda Hostonboarding Ssid Ippool.
+- Manage operations create and update of the resource Business Sda Hostonboarding Ssid Ippool.
+- Add SSID to IP Pool Mapping.
 - Update SSID to IP Pool Mapping.
 version_added: '4.0.0'
 extends_documentation_fragment:
@@ -33,19 +34,40 @@ requirements:
 - dnacentersdk >= 2.5.0
 - python >= 3.5
 seealso:
+- name: Cisco DNA Center documentation for Fabric Wireless AddSSIDToIPPoolMapping
+  description: Complete reference of the AddSSIDToIPPoolMapping API.
+  link: https://developer.cisco.com/docs/dna-center/#!add-ssid-to-ip-pool-mapping
 - name: Cisco DNA Center documentation for Fabric Wireless UpdateSSIDToIPPoolMapping2
   description: Complete reference of the UpdateSSIDToIPPoolMapping2 API.
   link: https://developer.cisco.com/docs/dna-center/#!update-ssid-to-ip-pool-mapping-2
 notes:
   - SDK Method used are
+    fabric_wireless.FabricWireless.add_ssid_to_ip_pool_mapping,
     fabric_wireless.FabricWireless.update_ssid_to_ip_pool_mapping2,
 
   - Paths used are
+    post /dna/intent/api/v1/business/sda/hostonboarding/ssid-ippool,
     put /dna/intent/api/v1/business/sda/hostonboarding/ssid-ippool,
 
 """
 
 EXAMPLES = r"""
+- name: Create
+  cisco.dnac.business_sda_hostonboarding_ssid_ippool:
+    dnac_host: "{{dnac_host}}"
+    dnac_username: "{{dnac_username}}"
+    dnac_password: "{{dnac_password}}"
+    dnac_verify: "{{dnac_verify}}"
+    dnac_port: "{{dnac_port}}"
+    dnac_version: "{{dnac_version}}"
+    dnac_debug: "{{dnac_debug}}"
+    state: present
+    scalableGroupName: string
+    siteNameHierarchy: string
+    ssidNames:
+    - string
+    vlanName: string
+
 - name: Update all
   cisco.dnac.business_sda_hostonboarding_ssid_ippool:
     dnac_host: "{{dnac_host}}"
@@ -68,11 +90,13 @@ RETURN = r"""
 dnac_response:
   description: A dictionary or list with the response returned by the Cisco DNAC Python SDK
   returned: always
-  type: dict
+  type: list
   sample: >
-    {
-      "executionId": "string",
-      "executionStatusURL": "string",
-      "message": "string"
-    }
+    [
+      {
+        "executionId": "string",
+        "executionStatusURL": "string",
+        "message": "string"
+      }
+    ]
 """
