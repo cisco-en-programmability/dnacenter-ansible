9e52aef322761c75bee7284b85d6353238ee1bd0
Nguyen Tan Thanh <104471718+ngtanthanh-qc@users.noreply.github.com>
2023-08-26 11:15:53 +0700

    Update dnac.py
    it seems `cmp_fn` is not used and there are multiple nested conditions. This is an optional enhancement

--- plugins/module_utils/dnac.py
+++ plugins/module_utils/dnac.py
@@ -91,26 +91,15 @@ def simple_cmp(obj1, obj2):
     return obj1 == obj2
 
 
-def get_dict_result(result, key, value, cmp_fn=simple_cmp):
+def get_dict_result(result, key, value):
     if isinstance(result, list):
-        if len(result) == 1:
-            if isinstance(result[0], dict):
-                result = result[0]
-                if result.get(key) is not None and result.get(key) != value:
-                    result = None
-            else:
-                result = None
-        else:
-            for item in result:
-                if isinstance(item, dict) and (item.get(key) is None or item.get(key) == value):
-                    result = item
-                    return result
-            result = None
-    elif not isinstance(result, dict):
-        result = None
-    elif result.get(key) is not None and result.get(key) != value:
-        result = None
-    return result
+        for item in result:
+            if isinstance(item, dict) and item.get(key) == value:
+                return item
+    elif isinstance(result, dict):
+        if result.get(key) == value:
+            return result
+    return None
 
 
 def dnac_argument_spec():
