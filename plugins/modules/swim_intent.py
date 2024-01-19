7cf9f3f9582b719806c1c689fbec07f784837288
Abhishek-121 <abmahesh@cisco.com>
2024-01-19 15:29:05 +0530

    remove unused log

--- plugins/modules/swim_intent.py
+++ plugins/modules/swim_intent.py
@@ -375,7 +375,6 @@ response:
 from ansible_collections.cisco.dnac.plugins.module_utils.dnac import (
     DnacBase,
     validate_list_of_dicts,
-    log,
     get_dict_result,
 )
 from ansible.module_utils.basic import AnsibleModule
