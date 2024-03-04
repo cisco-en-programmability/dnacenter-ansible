e4c8f120d72af6026a28116e9dfccd47702c6537
Abinash <abimishr@cisco.com>
2024-03-04 10:34:59 +0000

    Fixing Invalid SWIM issue while claiming

--- plugins/modules/pnp_intent.py
+++ plugins/modules/pnp_intent.py
@@ -474,7 +474,7 @@ class PnP(DnacBase):
         imageinfo = {
             'imageId': self.have.get('image_id')
         }
-
+        
         configinfo = {
             'configId': self.have.get('template_id'),
             'configParameters': [
@@ -485,7 +485,7 @@ class PnP(DnacBase):
             ]
         }
 
-        if configinfo["configId"] and self.validated_config[0]["template_params"]:
+        if configinfo.get("configId") and self.validated_config[0].get("template_params"):
             if isinstance(self.validated_config[0]["template_params"], dict):
                 if len(self.validated_config[0]["template_params"]) > 0:
                     configinfo["configParameters"] = []
@@ -609,7 +609,7 @@ class PnP(DnacBase):
                 number '{0}': {1}".format(self.want.get("serial_number"), str(device_response)), "DEBUG")
 
             if not (device_response and (len(device_response) == 1)):
-                self.log("Device with with serial number {0} is not found in the inventory".format(self.want.get("serial_number")), "WARNING")
+                self.log("Device with serial number {0} is not found in the inventory".format(self.want.get("serial_number")), "WARNING")
                 self.msg = "Adding the device to database"
                 self.status = "success"
                 self.have = have
@@ -893,6 +893,7 @@ class PnP(DnacBase):
                     params=self.want.get("pnp_params")[0],
                     op_modifies=True,
                 )
+                self.get_have().check_return_status()
                 self.have["deviceInfo"] = dev_add_response.get("deviceInfo")
                 self.log("Response from API 'add device' for single device addition: {0}".format(str(dev_add_response)), "DEBUG")
                 claim_params = self.get_claim_params()
@@ -905,8 +906,7 @@ class PnP(DnacBase):
                 )
 
                 self.log("Response from API 'claim a device to a site' for a single claiming: {0}".format(str(dev_add_response)), "DEBUG")
-                if claim_response.get("response") == "Device Claimed" \
-                        and self.have["deviceInfo"]:
+                if claim_response.get("response") == "Device Claimed" and self.have["deviceInfo"]:
                     self.result['msg'] = "Device Added and Claimed Successfully"
                     self.log(self.result['msg'], "INFO")
                     self.result['response'] = claim_response
