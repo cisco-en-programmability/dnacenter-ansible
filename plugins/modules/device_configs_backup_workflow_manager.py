a65246ccb84afa071cbfc8c93dde0a1ccba20108
Abinash <abimishr@cisco.com>
2024-05-02 14:55:21 +0000

    Resolving review comments for device configs backup

--- plugins/modules/device_configs_backup_workflow_manager.py
+++ plugins/modules/device_configs_backup_workflow_manager.py
@@ -63,7 +63,10 @@ options:
         description: Specifies the collection status of the device(s) on the GUI of Cisco Catalyst Center
         type: str
       file_path:
-        description: Location of the path or folder where the configs need to be exported in local system.
+        description:
+            - Location of the path or folder where the configs need to be exported in local system.
+            - If file_path is not given, it will store the backup file(s) in a folder named tmp in the
+              same directory of the playbook.
         type: str
         default: tmp
       file_password:
