ff307b5fe4b2a712c601cd036c475c2aa289c404
Syed-khadeerahmed <Syedkhadeerahmed12129@gmail.com>
2024-08-08 19:50:55 +0530

    untag test case compleated

--- tests/unit/modules/dnac/test_swim_workflow_manager.py
+++ tests/unit/modules/dnac/test_swim_workflow_manager.py
@@ -30,13 +30,8 @@ class TestswimWorkflowManager(TestDnacModule):
 
     test_data = loadPlaybookData("swim_workflow_manager")
 
-    playbook_import_image_url_tag_golden_load = test_data.get("playbook_import_image_url_tag_golden_load")
-    playbook_import_image_from_local_tag_golden = test_data.get("playbook_import_image_from_local_tag_golden")
-    playbook_tag_image_as_golden_and_load_on_device = test_data.get("playbook_tag_image_as_golden_and_load_on_device")
+
     playbook_untag_image_as_golden_and_load_on_device = test_data.get("playbook_untag_image_as_golden_and_load_on_device")
-    playbook_Distribute_image_associate_site_role = test_data.get("playbook_Distribute_image_associate_site_role")
-    playbook_Activate_image_associate_site_role = test_data.get("playbook_Activate_image_associate_site_role")
-    playbook_import_image_url_tag_golden = test_data.get("playbook_import_image_url_tag_golden")
     
 
     def setUp(self):
@@ -60,63 +55,24 @@ class TestswimWorkflowManager(TestDnacModule):
         """
         Load fixtures for user.
         """
-        # if "playbook_import_image_url_tag_golden" in self._testMethodName:
-        #     self.run_dnac_exec.side_effect = [
-        #         self.test_data.get("get_software_image_details"),
-        #         self.test_data.get("import_software_image_via_url"),
-        #         self.test_data.get("get_software_image_details_1"),
-        #         self.test_data.get("get_software_image_details_1"),
-        #         self.test_data.get("get_device_family_identifiers"),
-        #         self.test_data.get("get_software_image_details_1"),
-        #         self.test_data.get("get_golden_tag_status_of_an_image"),
-        #         self.test_data.get("tag_as_golden_image"),
-        #         self.test_data.get("get_software_image_details_1"),
-        #         self.test_data.get("get_device_family_identifiers1"),
-        #         self.test_data.get("get_software_image_details_2"),
-        #         self.test_data.get("get_software_image_details_3"),
-        #         self.test_data.get("get_golden_tag_status_of_an_image_2"),
-        #         self.test_data.get("import_image_url_tag_golden_response")
-        #     ]
         if "playbook_untag_image_as_golden_and_load_on_device" in self._testMethodName:
             self.run_dnac_exec.side_effect = [
-                self.test_data.get("get_software_image_details_untag_golden"),
-                self.test_data.get("get_site_untag_golden"),
-                self.test_data.get("get_device_family_identifiers_untag_golden"),
-                self.test_data.get("get_software_image_details_untag_golden_1"),
-                self.test_data.get("get_golden_tag_status_of_an_image_untag_golden"),
-                self.test_data.get("remove_golden_tag_for_image_untag_golden"),
-                self.test_data.get("get_software_image_details_untag_golden_2"),
-                self.test_data.get("get_site_untag_golden_1"),
-                self.test_data.get("get_device_family_identifiers_untag_golden_1"),
-                self.test_data.get("get_software_image_details_untag_golden_3"),
-                self.test_data.get("get_golden_tag_status_of_an_image_untag_golden_1"),
+                self.test_data.get("get_software_image_details"),
+                self.test_data.get("get_site"),
+                self.test_data.get("get_device_family_identifiers"),
+                self.test_data.get("get_software_image_details_1"),
+                self.test_data.get("get_golden_tag_status_of_an_image"),
+                self.test_data.get("remove_golden_tag_for_image"),
+                self.test_data.get("Task_details"),
+                self.test_data.get("get_software_image_details_2"),
+                self.test_data.get("get_site_1"),
+                self.test_data.get("get_device_family_identifiers_1"),
+                self.test_data.get("get_software_image_details_3"),
+                self.test_data.get("get_golden_tag_status_of_an_image_1"),
                 self.test_data.get("untag_image_as_golden_and_load_on_device_responce")
             ]
 
-    def test_user_role_workflow_manager_playbook_import_image_url_tag_golden(self):
-        """
-        Test case for user swim workflow manager when Import an image from a URL, tag it as golden.
-
-        This test case checks the behavior of the swim workflow when Import an image from a URL, tag it as golden in the specified Cisco Catalyst Center.
-        """
-        set_module_args(
-            dict(
-                dnac_host="1.1.1.1",
-                dnac_username="dummy",
-                dnac_password="dummy",
-                dnac_log=True,
-                state="merged",
-                config=self.playbook_import_image_url_tag_golden
-            )
-        )
-        result = self.execute_module(changed=True, failed=False)
-        print(result)
-        self.assertEqual(
-            result.get('response'),
-            "Tagging image  cat9k_iosxe.17.12.02.SPA.bin  golden for site  Global for family Cisco Catalyst 9300 Switch  for device deviceTag ALL successful."
-        )
-
-    def test_user_role_workflow_manager_playbook_untag_image_as_golden_and_load_on_device(self):
+    def test_swim_workflow_manager_playbook_untag_image_as_golden_and_load_on_device(self):
         """
         Test case for user role workflow manager when creating a user.
 
@@ -132,9 +88,14 @@ class TestswimWorkflowManager(TestDnacModule):
                 config=self.playbook_untag_image_as_golden_and_load_on_device
             )
         )
+        print(1)
         result = self.execute_module(changed=True, failed=False)
+        print(2)
         print(result)
         self.assertEqual(
             result.get('msg'),
-            "Untagging of image  cat9k_iosxe.17.12.02.SPA.bin for site  Global for family Cisco Catalyst 9300 Switch  for device deviceTag ALL successful."
+            "Untagging of image  cat9k_iosxe.17.12.02.SPA.bin for site  LTTS for family Cisco Catalyst 9000 UADP 8 Port Virtual Switch  for device deviceTag ALL successful."
         )
+
+
+
