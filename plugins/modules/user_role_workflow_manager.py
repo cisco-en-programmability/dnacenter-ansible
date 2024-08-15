27bf429f9620a2a14be7884e515992fd131ed476
Ajith Andrew J <ajithandrewj7@gmail.com>
2024-08-15 21:26:33 +0530

    bug fixed

--- plugins/modules/user_role_workflow_manager.py
+++ plugins/modules/user_role_workflow_manager.py
@@ -1097,7 +1097,7 @@ least three of the following classes: lowercase characters, uppercase characters
         self.validate_string_field(last_name, regex_name_validation,
                                    "last_name: '{0}' {1}".format(last_name, regex_name_validation_msg), error_messages)
 
-        email_regex = re.compile(r"[^@]+@[^@]+\.[^@]+")
+        email_regex = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,2}(?:\.[a-zA-Z]{2,2})*$")
         email = user_config.get("email")
         email_regex_msg = "email: Invalid email format for 'email': {0}".format(email)
         self.validate_string_field(email, email_regex, email_regex_msg, error_messages)
