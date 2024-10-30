9d6189c0426ba574db054b06ab0f67341da38825
md-rafeek <md.rafeek@gmail.com>
2024-10-30 14:51:14 +0530

    removed the assign device to site common code removed

--- plugins/module_utils/dnac.py
+++ plugins/module_utils/dnac.py
@@ -870,15 +870,6 @@ class DnacBase():
             Assigns the specified devices to the site. If the assignment is successful, returns True.
             Otherwise, logs an error and returns False along with error details.
         """
-        site_response = self.get_site(site_name)
-        if site_response.get("response") and site_response["response"][0].get("type"):
-            site_type = site_response["response"][0].get("type")
-            if site_type not in ("building", "floor"):
-                self.msg = "Device(s) can only be assigned to building/floor"
-                self.log(self.msg, "ERROR")
-                self.status = "failed"
-                self.module.fail_json(msg=self.msg)
-
         if self.get_ccc_version_as_integer() <= self.get_ccc_version_as_int_from_str("2.3.5.3"):
             try:
                 response = self.dnac_apply['exec'](
