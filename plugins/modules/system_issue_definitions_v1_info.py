158242b664b60b1fb8ffb342b9aa7516b49958c7
JosePabloOcampo1212 <jp.ocampo1000@gmail.com>
2024-11-19 07:14:18 -0600

    devel changes in module system_issue_definitions_v1_info

--- plugins/modules/system_issue_definitions_v1_info.py
+++ plugins/modules/system_issue_definitions_v1_info.py
@@ -95,15 +95,6 @@ options:
     description:
     - Order query parameter. The sort order of the field ascending or descending.
     type: str
-  id:
-    description: System Issue Definitions's id.
-    type: null
-  issueEnabled:
-    description: Issue Enabled.
-    type: bool
-  priority:
-    description: Priority.
-    type: str
   synchronizeToHealthThreshold:
     description: Synchronize To Health Threshold.
     type: bool
