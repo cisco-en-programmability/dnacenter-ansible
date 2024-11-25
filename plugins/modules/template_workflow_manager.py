af9895f1d6d8af554f5b49cda0e3d86d05231e67
Abhishek-121 <abmahesh@cisco.com>
2024-11-25 12:52:23 +0530

    add quotes to the variable

--- plugins/modules/template_workflow_manager.py
+++ plugins/modules/template_workflow_manager.py
@@ -2927,7 +2927,7 @@ class Template(DnacBase):
 
         filtered_device_list = []
         self.log(
-            "Filtering devices from the provided site-assigned device IDs: {0},  device_family='{1}', "
+            "Filtering devices from the provided site-assigned device IDs: '{0}',  device_family='{1}', "
             "and device_role='{2}'".format(
                 site_assign_device_ids,
                 device_family,
