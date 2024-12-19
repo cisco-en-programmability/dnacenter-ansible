cf7c61811314e4cd1c63c2b5ba05ea6d04e4245e
JosePabloOcampo1212 <jp.ocampo1000@gmail.com>
2024-12-19 12:51:25 -0600

    error sanity tests

--- plugins/action/icap_settings_configuration_models_preview_activity_id_network_devices_network_device_id_config_info.py
+++ plugins/action/icap_settings_configuration_models_preview_activity_id_network_devices_network_device_id_config_info.py
@@ -4,8 +4,6 @@
 from ansible.plugins.action import ActionBase
 from ansible_collections.cisco.dnac.plugins.action.icap_settings_configuration_models_preview_activity_id_network_devices_network_device_id_config_v1_info import ActionModule  # noqa: E501
 
-
-
 class ActionModule2(ActionBase):
 
     def __init__(self, *args, **kwargs):
