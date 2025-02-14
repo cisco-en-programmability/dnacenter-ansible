ccebb8a274394e90f141f044a16c17f89d75fa31
md-rafeek <md.rafeek@gmail.com>
2025-02-14 09:51:23 +0530

    PNP Device Documentation related changes fixed

--- plugins/modules/pnp_workflow_manager.py
+++ plugins/modules/pnp_workflow_manager.py
@@ -105,7 +105,7 @@ options:
         description:
           - Parameter values for parameterized templates.
           - Each variable has a value that needs to be passed as a key-value pair in the dictionary.
-          - Values should be provided as 'variable_name: variable_value'.
+          - Values should be provided as 'variable_name': 'variable_value'.
           - Supported for EWLC from Cisco Catalyst Center release version 2.3.7.x onwards.
         type: dict
         required: false
