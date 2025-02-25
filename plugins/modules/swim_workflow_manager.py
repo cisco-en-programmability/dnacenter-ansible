4f5a4080af34aa08da5fca36102511a1474e3e33
Madhan <madsanka@cisco.com>
2025-02-26 00:58:42 +0530

    Added example to support list of CCO images to import

--- plugins/modules/swim_workflow_manager.py
+++ plugins/modules/swim_workflow_manager.py
@@ -470,6 +470,26 @@ EXAMPLES = r"""
         cco_image_details:
             image_name: cat9k_iosxe.17.06.06a.SPA.bin
 
+- name: Import list of images from CCO (cisco.com)
+  cisco.dnac.swim_workflow_manager:
+    dnac_host: "{{dnac_host}}"
+    dnac_username: "{{dnac_username}}"
+    dnac_password: "{{dnac_password}}"
+    dnac_verify: "{{dnac_verify}}"
+    dnac_port: "{{dnac_port}}"
+    dnac_version: "{{dnac_version}}"
+    dnac_debug: "{{dnac_debug}}"
+    dnac_log_level: "{{dnac_log_level}}"
+    dnac_log: True
+    config:
+    - import_image_details:
+        type: CCO
+        cco_image_details:
+            image_name:
+              - cat9k_iosxe.17.16.01.SPA.bin
+              - C9800-SW-iosxe-wlc.17.16.01.SPA.bin
+              - C9800-80-universalk9_wlc.17.15.02b.SPA.bin
+
 - name: Tag the given image as golden and load it on device
   cisco.dnac.swim_workflow_manager:
     dnac_host: "{{dnac_host}}"
