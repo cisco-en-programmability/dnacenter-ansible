e9face17d714e4bb8291de7c5f6d6022724df6fb
Megha Kandari <megakandari134@gmail.com>
2025-03-23 13:34:48 +0530

    UT update for assurance issue

--- tests/unit/modules/dnac/test_assurance_issue_workflow_manager.py
+++ tests/unit/modules/dnac/test_assurance_issue_workflow_manager.py
@@ -17,13 +17,13 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 from unittest.mock import patch
-from ansible_collections.cisco.dnac.plugins.modules import assurance_issue_settings_workflow_manager
+from ansible_collections.cisco.dnac.plugins.modules import assurance_issue_workflow_manager
 from .dnac_module import TestDnacModule, set_module_args, loadPlaybookData
 
 
 class TestDnacAssuranceSettings(TestDnacModule):
-    module = assurance_issue_settings_workflow_manager
-    test_data = loadPlaybookData("assurance_issue_settings_workflow_manager")
+    module = assurance_issue_workflow_manager
+    test_data = loadPlaybookData("assurance_issue_workflow_manager")
     playbook_config_updation = test_data.get("playbook_config_updation")
     playbook_config_creation = test_data.get("playbook_config_creation")
     playbook_config_deletion = test_data.get("playbook_config_deletion")
@@ -76,6 +76,21 @@ class TestDnacAssuranceSettings(TestDnacModule):
                 self.test_data.get("exist_after_creation"),
             ]
 
+        if "invalid_severity" in self._testMethodName:
+            self.run_dnac_exec.side_effect = [
+                self.test_data.get("Testing_creation_exit"),
+            ]
+
+        if "invalid_duration" in self._testMethodName:
+            self.run_dnac_exec.side_effect = [
+                self.test_data.get("Testing_creation_exit"),
+            ]
+
+        if "invalid_priority" in self._testMethodName:
+            self.run_dnac_exec.side_effect = [
+                self.test_data.get("Testing_creation_exit"),
+            ]
+
         if "deletion" in self._testMethodName:
             self.run_dnac_exec.side_effect = [
                 self.test_data.get("get_API_response_deletion"),
@@ -121,7 +136,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
                 self.test_data.get("get_issue_ids_resolution"),
             ]
 
-    def test_assurance_issue_settings_workflow_manager_updation(self):
+    def test_assurance_issue_workflow_manager_updation(self):
         """
         Test case for updating Assurance Issue Settings in Cisco DNA Center.
         Verifies that the workflow manager correctly updates user-defined issue settings
@@ -148,7 +163,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
               'duration_in_minutes': 2}], 'is_enabled': True, 'priority': 'P2', 'is_notification_enabled': True, 'prev_name': 'test_seema_1'}}
         )
 
-    def test_assurance_issue_settings_workflow_manager_creation(self):
+    def test_assurance_issue_workflow_manager_creation(self):
         """
         Test case for creating Assurance Issue Settings in Cisco DNA Center.
         Verifies that the workflow manager correctly creates user-defined issue settings
@@ -175,7 +190,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
                         'occurrences': 1, 'duration_in_minutes': 2}], 'is_enabled': True, 'priority': 'P2', 'is_notification_enabled': True}}
         )
 
-    def test_assurance_issue_settings_workflow_manager_deletion(self):
+    def test_assurance_issue_workflow_manager_deletion(self):
         """
         Test case for deleting Assurance Issue Settings in Cisco DNA Center.
         Verifies that the workflow manager correctly deletes user-defined issue settings
@@ -200,7 +215,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
             {'ippo': 'Assurance user issue deleted successfully'}
         )
 
-    def test_assurance_issue_settings_workflow_manager_update_system_issue(self):
+    def test_assurance_issue_workflow_manager_update_system_issue(self):
         """
         Test case for updating system issues in Assurance Issue Settings.
         Verifies that the workflow manager correctly updates system-defined issues
@@ -225,7 +240,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
             {'AP Reboot Crash': 'System issue Updated Successfully'}
         )
 
-    def test_assurance_issue_settings_workflow_manager_command_execution(self):
+    def test_assurance_issue_workflow_manager_command_execution(self):
         """
         Test case for executing commands in Assurance Issue Settings.
         Verifies that the workflow manager correctly processes and executes
@@ -244,13 +259,13 @@ class TestDnacAssuranceSettings(TestDnacModule):
             )
         )
         result = self.execute_module(changed=True, failed=False)
-        print(result['response']['input_isssue_config'])
+        print(result['response'])
         self.assertEqual(
-            result['response']['input_isssue_config'],
+            result['response']['input_issue_config'],
             [{'issue_name': 'jan8_1', 'issue_process_type': 'command_execution'}]
         )
 
-    def test_assurance_issue_settings_workflow_manager_No_data_found(self):
+    def test_assurance_issue_workflow_manager_No_data_found(self):
         """
         Test case for handling no data scenario in Assurance Issue Settings.
         Verifies that the workflow manager correctly identifies and reports
@@ -275,7 +290,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
             "No data received for the issue: {'issue_name': 'jan8_1', 'issue_process_type': 'resolution'}"
         )
 
-    def test_assurance_issue_settings_workflow_manager_resolution(self):
+    def test_assurance_issue_workflow_manager_resolution(self):
         """
         Test case for verifying issue resolution in Assurance Issue Settings.
         Ensures that the workflow manager correctly processes and confirms
@@ -300,7 +315,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
             "Issue resolution verified successfully for '[{'issue_name': 'Rangatestlink', 'issue_process_type': 'resolution'}]'."
         )
 
-    def test_assurance_issue_settings_workflow_manager_ignore(self):
+    def test_assurance_issue_workflow_manager_ignore(self):
         """
         Test case for ignoring an assurance issue in the workflow manager.
         Ensures that the specified assurance issue is correctly processed
@@ -315,17 +330,14 @@ class TestDnacAssuranceSettings(TestDnacModule):
                 state="merged",
                 dnac_version="2.3.7.9",
                 config=self.playbook_config_ignore,
-                config_verify=True
+                # config_verify=True
             )
         )
-        result = self.execute_module(changed=False, failed=True)
-        print(result['response']['input_isssue_config'])
-        self.assertEqual(
-            result['response']['input_isssue_config'],
-            [{'issue_name': 'Excessive time lag between Cisco Catalyst Center and device "DC-T-9300"', 'issue_process_type': 'ignore'}]
-        )
+        result = self.execute_module(changed=True, failed=False)
+        print(result['msg'])
+        self.assertIn("Issue ignored successfully.", result['msg'])
 
-    def test_assurance_issue_settings_workflow_manager_invalid_severity(self):
+    def test_assurance_issue_workflow_manager_invalid_severity(self):
         """
         Test case to validate behavior when an invalid severity is provided in the assurance issue settings workflow manager.
         This test ensures that the module handles invalid severity values in the playbook config correctly by
@@ -336,7 +348,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
                 dnac_host="1.1.1.1",
                 dnac_username="dummy",
                 dnac_password="dummy",
-                dnac_version="2.3.7.6",
+                dnac_version="2.3.7.9",
                 dnac_log=True,
                 state="merged",
                 config_verify=True,
@@ -344,9 +356,13 @@ class TestDnacAssuranceSettings(TestDnacModule):
             )
         )
         result = self.execute_module(changed=False, failed=True)
-        self.assertIn("Invalid parameters in playbook config", result['response'])
+        print(result['msg'])
+        self.assertEqual(
+            result['msg'],
+            "Invalid severity value 'critic' . Allowed values are: Emergency, Alert, Critical, Error, Warning, Notice, Info."
+        )
 
-    def test_assurance_issue_settings_workflow_manager_invalid_duration(self):
+    def test_assurance_issue_workflow_manager_invalid_duration(self):
         """
         Test case to validate behavior when an invalid duration is provided in the assurance issue settings workflow manager.
         This test ensures that the module handles invalid duration values in the playbook config correctly by
@@ -367,7 +383,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
         result = self.execute_module(changed=False, failed=True)
         self.assertIn("Invalid parameters in playbook config", result['response'])
 
-    def test_assurance_issue_settings_workflow_manager_invalid_priority(self):
+    def test_assurance_issue_workflow_manager_invalid_priority(self):
         """
         Test case to validate behavior when an invalid priority is provided in the assurance issue settings workflow manager.
         This test ensures that the module handles invalid priority values in the playbook config correctly by
@@ -388,7 +404,7 @@ class TestDnacAssuranceSettings(TestDnacModule):
         result = self.execute_module(changed=False, failed=True)
         self.assertIn("Invalid parameters in playbook config", result['response'])
 
-    def test_assurance_issue_settings_workflow_manager_invalid_time_format(self):
+    def test_assurance_issue_workflow_manager_invalid_time_format(self):
         """
         Test case to validate behavior when an invalid time format is provided in the assurance issue settings workflow manager.
         This test ensures that the module correctly handles invalid time formats, such as an incorrect date or time,
