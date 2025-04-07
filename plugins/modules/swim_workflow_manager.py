3fd4a0212d8854e6d5e38c9eb555bf168d1e1193
skesali <sonalikesali777@gmail.com>
2025-04-07 17:58:51 +0530

    version based changes

--- plugins/modules/swim_workflow_manager.py
+++ plugins/modules/swim_workflow_manager.py
@@ -2929,6 +2929,14 @@ def main():
     ccc_swims = Swim(module)
     state = ccc_swims.params.get("state")
 
+    if ccc_swims.compare_dnac_versions(ccc_swims.get_ccc_version(), "2.3.5.3") < 0:
+        ccc_swims.msg = (
+            "The specified version '{0}' does not support the 'swim_workflow_manager' feature. Supported versions start from '2.3.5.3' onwards. "
+            .format(ccc_swims.get_ccc_version())
+        )
+        ccc_swims.status = "failed"
+        ccc_swims.check_return_status()
+
     if state not in ccc_swims.supported_states:
         ccc_swims.status = "invalid"
         ccc_swims.msg = "State {0} is invalid".format(state)
