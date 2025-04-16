4e2db4e6c47dd1747f5b3e0f5b9747f2ff9840b9
Archit Soni <soni.archit03@gmail.com>
2025-04-16 11:20:39 +0530

    Minor change in authors.

--- plugins/modules/template_workflow_manager.py
+++ plugins/modules/template_workflow_manager.py
@@ -7,7 +7,7 @@
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
-__author__ = ['Madhan Sankaranarayanan, Rishita Chowdhary, Akash Bhaskaran, Muthu Rakesh, Abhishek Maheshwari', 'Archit Soni']
+__author__ = ['Madhan Sankaranarayanan, Rishita Chowdhary, Akash Bhaskaran, Muthu Rakesh, Abhishek Maheshwari, Archit Soni']
 
 DOCUMENTATION = r"""
 ---
