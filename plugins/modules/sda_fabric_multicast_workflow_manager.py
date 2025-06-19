b968a76a89f98839a12b47af6b82cecb36b4f8db
Archit Soni <soni.archit03@gmail.com>
2025-06-19 16:16:48 +0530

    The parameter ip_pool_name was not required for multicast deletion cases when ASM or SSM configuration is not involved. Keeping it mandatory was causing non-idempotency.

--- plugins/modules/sda_fabric_multicast_workflow_manager.py
+++ plugins/modules/sda_fabric_multicast_workflow_manager.py
@@ -2382,12 +2382,29 @@ class FabricMulticast(DnacBase):
                 if have_fabric_multicast_exists:
                     ip_pool_name = have_multicast_details.get("ipPoolName")
                 else:
-                    self.msg = "The parameter 'ip_pool_name' is missing for the fabric with name '{fabric_name}'.".format(
-                        fabric_name=fabric_name
-                    )
-                    self.set_operation_result(
-                        "failed", False, self.msg, "ERROR"
-                    ).check_return_status()
+                    state = self.params.get("state")
+                    if state == "deleted":
+                        #  Deleted Case: ip_pool_name is mandatory if ssm or asm config change is required.
+                        self.log(f"Checking if asm or ssm config change is required for the fabric site '{fabric_name}'.", "DEBUG")
+                        if item.get("ssm") or item.get("asm"):
+                            self.msg = (
+                                f"The parameter 'ip_pool_name' is mandatory for the fabric with name '{fabric_name}' "
+                                "when the state is 'deleted' and the 'ssm' or 'asm' configuration is provided."
+                            )
+                            self.fail_and_exit(self.msg)
+
+                        # Deleted Case: ip_pool_name is not mandatory when deleting entire multicast.
+                        self.log(
+                            f"The parameter 'ip_pool_name' is not mandatory for the fabric with name '{fabric_name}' "
+                            "when the state is 'deleted' and the 'ssm' or 'asm' configuration is not provided.",
+                            "DEBUG",
+                        )
+                    else:
+                        # Merged Case
+                        self.msg = f"The parameter 'ip_pool_name' is missing for the fabric with name '{fabric_name}'."
+                        self.set_operation_result(
+                            "failed", False, self.msg, "ERROR"
+                        ).check_return_status()
 
             is_valid_reserved_pool = self.check_valid_reserved_pool(
                 ip_pool_name, fabric_name
