cc4633ef5730e35938b1b9fe3a859edd7b032573
SyedKhadeerAhmed <syedkahm@cisco.com>
2025-07-09 18:59:44 +0530

    UT completed

--- tests/unit/modules/dnac/test_provision_workflow_manager.py
+++ tests/unit/modules/dnac/test_provision_workflow_manager.py
@@ -220,7 +220,7 @@ class TestDnacProvisionWorkflow(TestDnacModule):
                 config=self.playbook_reprovision_wired_device
             )
         )
-        result = self.execute_module(changed=True, failed=False)       
+        result = self.execute_module(changed=True, failed=False)
         print(result)
         self.assertEqual(
             result.get('msg'),
